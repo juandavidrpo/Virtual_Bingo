bca37f631d6dba83118dc922d3d8d840279f165f
Juan David <juandavid-1527@hotmail.com>
2022-03-13 18:23:45 -0500

    Exception is created

---
+++ Backend/src/main/java/com/bingo/exception/Exception.java
@@ -0,0 +1,12 @@
+package com.bingo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class Exception extends RuntimeException {
+
+    public Exception(String message) {
+        super(message);
+    }
+}
